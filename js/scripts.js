84aa77e6c34a300ed51c50b58a3ae9a459908eb5
Ondicho <ondicho53@gmail.com>
2020-07-30 22:24:50 +0300

    this commit adds an order function in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function order(type,size,crust,toppings,number,delivery){
+    this.type=type;
+    this.size=size;
+    this.crust=crust;
+    this.toppings=toppings;
+    this.number=number;
+    this.delivery=delivery;
+}
