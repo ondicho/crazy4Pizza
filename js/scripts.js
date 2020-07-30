adb78a287ff9f594f29c0af79c7aa0a096df04b7
Ondicho <ondicho53@gmail.com>
2020-07-30 23:11:05 +0300

    this commit changes the delivery select list option to a check box,it also declares array variables for the different pizza properties

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,7 @@ function order(type,size,crust,toppings,number,delivery){
     this.number=number;
     this.delivery=delivery;
 }
+var type=["Hawaiian","BBQ Chicken","Chicken Periperi","BBQ Beef"];
+var size=["small","medium","large"];
+var crust=["crispy","stuffed","Gluten Free"];
+var toppings=["cheese","mushroom","beef","chicken"]
