0c56f330dccd527c5a91586e5c2be003c286f186
Ondicho <ondicho53@gmail.com>
2020-08-04 15:50:21 +0300

    this commit displays output in my order tab

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //business logic
-function order(pizzaType,pizzaSize,pizzCrust,pizzaToppings,pizzaNumber,deliveryCheck){
+function order(pizzaType,pizzaSize,pizzaCrust,pizzaToppings,pizzaNumber,deliveryCheck){
     this.pizzaType=pizzaType;
     this.pizzaSize=pizzaSize;
     this.pizzaCrust=pizzaCrust;
@@ -8,49 +8,27 @@ function order(pizzaType,pizzaSize,pizzCrust,pizzaToppings,pizzaNumber,deliveryC
     this.deliveryCheck=deliveryCheck;
 }
 
-order.prototype.fullOrder = function() {
-  return this.pizzaType + " " + this.pizzaSize + " " + this.pizzaCrust + " " + this.pizzaToppings + " " + this.pizzaNumber + " " + this.deliveryCheck;
-}
-var pizzaType=["Hawaiian","BBQ Chicken","Chicken Periperi","BBQ Beef"];
-var pizzaSize=["small","medium","large"];
-var pizzCrust=["crispy","stuffed","Gluten Free"];
-var pizzaToppings=["cheese","mushroom","beef","chicken"]
-
-const pizzaSizeLarge=1200;
-const pizzaSizeMedium=900;
-const pizzaSizeSmall=650;
-const pizzaToppings=150;
-const deliveryFee=250;
+var pizzaSizeLarge=1200;
+var pizzaSizeMedium=900;
+var pizzaSizeSmall=650;
+const pizza_Toppings=150;
+const delivery=250;
 
 //user interface logic
 
 $(document).ready(function() {
-  $("form#orderButton").submit(function(event) {
-      console.log(pizzaType);
-      event.preventDefault();
+  $("#orderButton").click(function() {
 
-      console.log(fullOrder);
+      pizzaType=$("#pizzaType").val();
+      pizzaSize=$("#pizzaSize").val();
+      pizzaCrust=$("#pizzaCrust").val();
+      pizzaToppings=$("#pizzaToppings").val();
+      pizzaNumber=$("#pizzaNumber").val();
+      deliveryCheck=$("#deliveryCheck").val();
 
-
-
-      var inputtedType=$("orders#pizzaType").val();
-      var inputtedSize=$("orders#pizzaSize").val();
-      var inputtedCrust=$("orders#pizzaCrust").val();
-      var inputtedToppings=$("orders#pizzaToppings").val();
-      var inputtedNumber=$("orders#pizzaNumber").val();
-      var inputtedDelivery=$("orders#deliveryCheck").val();
-      var newOrder=new order(inputType, inputType,  inputCrust, inputToppings,  inputNumber,  inputDelivery);
+      var myOrder=new order(pizzaType, pizzaSize, pizzaCrust, pizzaToppings, pizzaNumber, deliveryCheck);
+  $("#myOrder").append(myOrder.pizzaType + " " + myOrder.pizzaSize + " " + myOrder.pizzaCrust + " " + myOrder.pizzaToppings + " " + myOrder.pizzaNumber + " " + myOrder.deliveryCheck);
 
 
-  $("#myOrder").append(order.fullOrder());
-
-  $("input#pizzaType").val("");
-  $("input#pizzaSize").val("");
-  $("input#pizzaCrust").val("");
-  $("input#pizzaToppings").val("");
-  $("input#pizzaNumber").val("");
-  $("input#deliveryCheck").val("");
-
-
-  });
+});
 });
